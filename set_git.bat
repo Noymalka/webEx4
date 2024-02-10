f4ff81115e3667ad66f4c2fa92602cc7eb831ed0
Noymalka <Noy.malka@e.braude.ac.il>
2024-02-10 14:33:02 +0200

    first commit

---
+++ set_git.bat
@@ -0,0 +1,11 @@
+@echo off
+:: this bat file is used to set up a git repository
+:: to run, use .\set_git.bat
+
+:: Uncomment the following line to initialize the repository
+ git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Noymalka/webEx4.git
+git push -u origin main
